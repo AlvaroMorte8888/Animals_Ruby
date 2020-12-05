2071ac22856d191eaabfbf9e65cf774ea8f5dfe9
AlvaroMorte <alvaromorte8888@gmail.com>
2020-12-06 00:52:51 +0200

    Add JavaScript models for me Project

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,10 @@
 const { environment } = require('@rails/webpacker')
 
+const webpacker = require("webpack")
+environment.plugin.append("Provide", new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'defoult']
+}))
+
 module.exports = environment
