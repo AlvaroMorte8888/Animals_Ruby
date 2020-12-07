ef73b782ebd3834517b735d2a9a9d59d41164be4
AlvaroMorte <alvaromorte8888@gmail.com>
2020-12-07 17:58:22 +0200

    modefication new.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'posts/index'
-  get 'posts/:id', to: 'posts#show'
   get 'posts/new', to: 'posts#new'
+  get 'posts/:id', to: 'posts#show'
   post 'posts/create', to: 'posts#create'
 
   root to: "posts#index"
